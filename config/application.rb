551c49deb2e27dd034bae0db4f638d2f1cdf9256
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-08-16 21:38:37 +0200

    Heroku not workingé

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,9 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-if ['development', 'test'].include? ENV['RAILS_ENV']
-  Dotenv::Railtie.load
-end
+# if ['development', 'test'].include? ENV['RAILS_ENV']
+#   Dotenv::Railtie.load
+# end
 
 
 module ThpGossipProject
